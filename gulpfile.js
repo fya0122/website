ead9f4d4d10d3a42ab101b21fa63673236d91bfc
fya0122 <18730930122@163.com>
2019-01-30 16:18:18 +0800

    '改了很多'

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
 const gulp = require('gulp')
+const gulpPostcss = require('gulp-postcss')
+const postcssPx2rem = require('postcss-px2rem')
 gulp.task('default', (() => {
+	const processors = [postcssPx2rem({ remUnit: 75 })]
+	return gulp.src('./src/css/mobile.css').pipe(gulpPostcss(processors)).pipe(gulp.dest('./dist/css/'))
 }))
