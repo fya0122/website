9b9fe9ff98f03d911ce904ea9c2de67a9035854c
fya0122 <18730930122@163.com>
2019-01-31 23:55:42 +0800

    'ok'

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 const gulp = require('gulp')
 const gulpPostcss = require('gulp-postcss')
 const postcssPx2rem = require('postcss-px2rem')
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 gulp.task('default', (() => {
-	const processors = [postcssPx2rem({ remUnit: 75 })]
+	const processors = [
+	  postcssPx2rem({ remUnit: 75 }),
+	  autoprefixer(),
+	  cssnano()
+	]
 	return gulp.src('./src/css/mobile.css').pipe(gulpPostcss(processors)).pipe(gulp.dest('./dist/css/'))
 }))
